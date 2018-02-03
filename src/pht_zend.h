0cd08c51aec4b836514ee7cebcd8eaba032268f6
Thomas Punt <tpunt@php.net>
2018-02-03 17:07:32 +0000

    Remove ZEND_API from function declarations

--- src/pht_zend.h
+++ src/pht_zend.h
@@ -19,9 +19,9 @@
 #ifndef PHT_ZEND_H
 #define PHT_ZEND_H
 
-ZEND_API HashTable* ZEND_FASTCALL pht_zend_array_dup(HashTable *source);
-ZEND_API zend_ast *pht_zend_ast_copy(zend_ast *ast);
-ZEND_API void ZEND_FASTCALL _pht_zval_copy_ctor_func(zval *zvalue ZEND_FILE_LINE_DC);
+HashTable* ZEND_FASTCALL pht_zend_array_dup(HashTable *source);
+zend_ast *pht_zend_ast_copy(zend_ast *ast);
+void ZEND_FASTCALL _pht_zval_copy_ctor_func(zval *zvalue ZEND_FILE_LINE_DC);
 
 /*
 A thread-safe version of ZVAL_DUP, where constant AST and array types are
